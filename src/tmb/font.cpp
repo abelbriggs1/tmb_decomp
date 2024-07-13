bc0b310ef698b43b89b23de3c2dcea32075a8cee
Abel Briggs <abelbriggs1@hotmail.com>
2024-07-13 20:05:32 +0000

    Begin matching some functions

--- src/tmb/font.cpp
+++ src/tmb/font.cpp
@@ -1,5 +1,34 @@
 #include "common.h"
 
+extern "C" {
+extern char* strchr(const char*, int);
+extern unsigned int strlen(const char*);
+}
+
+#define NUM_FONTS 2
+
+typedef struct {
+    u16 unk1;
+    u16 unk2;
+    u16 char_width;
+    u16 unk4;
+    u16 size_lsh_6_div_5;
+    u16 size_lsh_3;
+    u16 unk5;
+    u16 spacing;
+    u16 x_subpixel;
+    u16 y_subpixel;
+    u16 unk6;
+    u16 unk7;
+    u8 r;
+    u8 g;
+    u8 b;
+    u8 a;
+    u32 unk8;
+} _fontInfo;
+
+extern _fontInfo fontInfo[NUM_FONTS];
+
 INCLUDE_ASM("/workspace/asm/nonmatchings/tmb/font", fontInit__F10_vramAddrs);
 
 INCLUDE_ASM("/workspace/asm/nonmatchings/tmb/font", fontDmaFontData__Fv);
@@ -18,15 +47,45 @@ INCLUDE_ASM("/workspace/asm/nonmatchings/tmb/font", fontSetCutOut__Fi);
 
 INCLUDE_ASM("/workspace/asm/nonmatchings/tmb/font", fontSetDefaultColor__Fi);
 
-INCLUDE_ASM("/workspace/asm/nonmatchings/tmb/font", fontSetSize__Fii);
+void fontSetSize(int font, int size)
+{
+    if (font >= NUM_FONTS) {
+        return;
+    }
 
-INCLUDE_ASM("/workspace/asm/nonmatchings/tmb/font", fontSetCharWidth__Fii);
+    if (font < 0) {
+        return;
+    }
+
+    fontInfo[font].size_lsh_3 = (size * 8);
+    fontInfo[font].size_lsh_6_div_5 = (size * 64) / 5;
+}
+
+void fontSetCharWidth(int font, int char_width)
+{
+    fontInfo[font].char_width = char_width;
+}
 
 INCLUDE_ASM("/workspace/asm/nonmatchings/tmb/font", fontSetDefaultSize__Fi);
 
-INCLUDE_ASM("/workspace/asm/nonmatchings/tmb/font", fontSetSpacing__Fii);
+void fontSetSpacing(int font, int spacing)
+{
+    fontInfo[font].spacing = fontInfo[font].size_lsh_6_div_5 + (spacing * 16);
+}
 
-INCLUDE_ASM("/workspace/asm/nonmatchings/tmb/font", fontStringWidth__FiPc);
+int fontStringWidth(int font, char* str)
+{
+    int str_len;
+
+    char* cv1 = strchr(str, '\n');
+
+    if (cv1 == NULL) {
+        str_len = strlen(str);
+        return str_len * fontInfo[font].spacing;
+    } else {
+        return (cv1 - str) * fontInfo[font].spacing;
+    }
+}
 
 INCLUDE_ASM("/workspace/asm/nonmatchings/tmb/font", fontSpritePrintXY__FiiiPc);
 
