b188ceb0b72b5985871a5ae844fe0cfbe50ed510
Abel Briggs <abelbriggs1@hotmail.com>
2025-12-20 13:25:22 -0600

    Revert to using `asm_differ` for now
    `objdiff` doesn't quite fit our use case yet, and Splat has a couple
    of outstanding issues for PS2 integration specifically which make
    it a non-starter. Wait awhile for support to mature.

--- diff_settings.py
+++ diff_settings.py
@@ -1,7 +1,7 @@
 def apply(config, args):
     config["arch"] = "mipsee"
-    config["baseimg"] = "disks/SCUS_971.01"
-    config["myimg"] = "build/SCUS_971.01"
+    config["baseimg"] = "disks/SCUS_971.01.rom"
+    config["myimg"] = "build/SCUS_971.01.rom"
     config["mapfile"] = "build/SCUS_971.01.map"
     config["source_directories"] = ["."]
     config["objdump_executable"] = "mips-linux-gnu-objdump"
