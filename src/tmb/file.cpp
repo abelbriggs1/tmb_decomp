788502d418d5bbd6a3c66b8733de86e50a7c50cd
Abel Briggs <abelbriggs1@hotmail.com>
2026-03-01 16:01:14 -0600

    tmb/file: Begin decompiling `tmb/font.cpp`

---
+++ src/tmb/file.cpp
@@ -0,0 +1,99 @@
+#include "common.h"
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", fileReadf__FPcPv);
+
+INCLUDE_RODATA("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", D_004F20F8);
+
+INCLUDE_RODATA("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", D_004F2108);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", fileReads__FPcPvUi);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", fileWritef__FPcPvi);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", fileInitializeCd__Fv);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", getTexFilesLoaded__Fv);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", getNgpFilesLoaded__Fv);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", getResFilesLoaded__Fv);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", getNextNgpLoadAddr__Fv);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", getNextTexLoadAddr__Fv);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", getNextResLoadAddr__Fv);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", getNgpAddr__Fi);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", getTexAddr__Fi);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", getResAddr__Fi);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", getGenericName__Fi);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", getMaxTexId__Fi);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", getMaxTexAddr__Fi);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", getMaxResAddr__Fi);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", setMaxTexId__FiUs);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", setMaxTexAddr__FiUs);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", setMaxResAddr__FiUs);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", getIdxOfAddr__FPc);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", getIdxOfResAddr__FPc);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", getIdxOfName__FPc);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", fileAddNgpFile__FPci);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", fileAddTexFile__FPci);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", fileAddResFile__FPci);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", fileAddName__FPc);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", fileOnlyNgpFile__FPci);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", fileOnlyTexFile__FPci);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", fileOnlyResFile__FPci);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", fileInitBeforeDbLoad__Fv);
+
+void filePrintFileStatus()
+{
+    // Debug function, stubbed out of the NTSC release.
+}
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", fileMakeDirTree__Fv);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", fourCharsToInt__Fi);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", twoCharsToShort__Fi);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", fileCdRead__FllPc);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file",
+    fileCdSearchFile__FP10sceCdlFILEPCc);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", fileHierAddrOfSect__FUi);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file",
+    filePrintCdFiles__FP13_cdFileSystemi);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", fileStringCompare__FPcT0);
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", fileTrimPath__FPc);
+
+void fileOutputTime()
+{
+    // Debug function, stubbed out of the NTSC release.
+}
+
+INCLUDE_ASM("/mnt/brahms/projects/tmb-decomp/asm/nonmatchings/tmb/file", fileGetTimeString__Fv);
